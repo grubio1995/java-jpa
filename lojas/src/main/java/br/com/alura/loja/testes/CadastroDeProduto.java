9f289afe29f6c9bab7fe2e3fa32f780e77f1d8ef
Gabriel Rubio <gabrielpinhatarubio@gmail.com>
2022-05-11 22:31:15 -0300

    Organizando o código

--- lojas/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ lojas/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import br.com.alura.loja.dao.ProdutoDao;
 import br.com.alura.loja.modelo.Produto;
 
 public class CadastroDeProduto {
@@ -20,9 +21,11 @@ public class CadastroDeProduto {
 		EntityManagerFactory factory = Persistence.createEntityManagerFactory("lojas");
 		
 		EntityManager em = factory.createEntityManager();
+
+		ProdutoDao produtoDao = new ProdutoDao(em);
 		
 		em.getTransaction().begin();
-		em.persist(celular);
+		produtoDao.cadastrar(celular);
 		em.getTransaction().commit();
 		em.close();
 	}
