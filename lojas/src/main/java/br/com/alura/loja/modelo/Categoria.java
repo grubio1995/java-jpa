28f7145ea0ebcf11e00301ee09a87ca8f09b01af
Gabriel Rubio <gabrielpinhatarubio@gmail.com>
2022-05-12 21:41:30 -0300

    Mapeando relacionamentos

--- lojas/src/main/java/br/com/alura/loja/modelo/Categoria.java
+++ lojas/src/main/java/br/com/alura/loja/modelo/Categoria.java
@@ -1,8 +1,27 @@
 package br.com.alura.loja.modelo;
 
-public enum Categoria {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-	CELULARES,
-	INFORMAICA,
-	LIVROS;
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+
+	public Categoria(String nome) {
+		this.nome = nome;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	
 }
--- lojas/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ lojas/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 
 import javax.persistence.EntityManager;
 
+import br.com.alura.loja.dao.CategoriaDao;
 import br.com.alura.loja.dao.ProdutoDao;
 import br.com.alura.loja.modelo.Categoria;
 import br.com.alura.loja.modelo.Produto;
@@ -13,13 +14,16 @@ public class CadastroDeProduto {
 
 	public static void main(String[] args) {
 	
-		Produto celular = new Produto("Xiaomi Redmi", "Muito legal",new BigDecimal("800"), Categoria.CELULARES);
+		Categoria celulares = new Categoria("CELULARES");
+		Produto celular = new Produto("Xiaomi Redmi", "Muito legal",new BigDecimal("800"), celulares);
 		
 		EntityManager em = JpaUtil.getEntityManager();
 
+		CategoriaDao categoriaDao = new CategoriaDao(em);
 		ProdutoDao produtoDao = new ProdutoDao(em);
 		
 		em.getTransaction().begin();
+		categoriaDao.cadastrar(celulares);
 		produtoDao.cadastrar(celular);
 		em.getTransaction().commit();
 		em.close();
