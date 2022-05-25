13fb4baf3d20fba4c7af90a905dd70817e479c98
Gabriel Rubio <gabrielpinhatarubio@gmail.com>
2022-05-24 22:05:59 -0300

    Mapeando novas enidades

---
+++ lojas/src/main/java/br/com/alura/loja/modelo/Cliente.java
@@ -0,0 +1,53 @@
+package br.com.alura.loja.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String nome;
+	private String cpf;
+
+	public Cliente() {
+	}
+
+	public Cliente(String nome, String cpf) {
+		this.nome = nome;
+		this.cpf = cpf;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	
+
+}
