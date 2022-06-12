168e6df9f40374811bc33e76276f3fa2a2d62540
Gabriel Rubio <gabrielpinhatarubio@gmail.com>
2022-06-12 18:25:37 -0300

    Mapeamento de herança - Joined

--- lojas/src/main/java/br/com/alura/loja/modelo/Produto.java
+++ lojas/src/main/java/br/com/alura/loja/modelo/Produto.java
@@ -17,7 +17,7 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "produtos")
 @NamedQuery(name = "Produto.produtosPorCategoria", query = "SELECT p FROM Produto p where p.nome LIKE :nome")
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@Inheritance(strategy = InheritanceType.JOINED )
 public class Produto {
 
 	@Id
